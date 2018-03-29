fedd74a26fa5465354750f42b065276136653ebb
Raj Patel <raj312.raj312@gmail.com>
2018-03-28 20:41:24 -0400

    Display additional information about the location in a table

--- myscript.js
+++ myscript.js
@@ -7,7 +7,11 @@ $(function() {
 
 }) //End of document ready
 
+var jsonData; //global var
+
 function loadData(data){
+	jsonData = data.stationBeanList;
+
 	var stationList = data.stationBeanList;
 	//loop through all the data and 
 
@@ -34,3 +38,54 @@ function loadData(data){
 
 
 }
+
+
+//update a customer
+$(document).on("pageshow", "#moreDetails", function() {
+	//clear the table
+	$("#extraInfo").html("");
+
+	rowid  = localStorage.getItem("rowid");
+	//fetch data with a specific id
+	jsonData.forEach(d => {
+		if(d.id == rowid){
+			$("#extraInfo").append(
+			"<tr>" +
+				"<th> Station Name</th> " +
+				" <td> " + d.stationName + "</td>" +
+			"</tr>" +
+			"<tr>" +
+				"<th> Available Docks</th> " +
+				" <td> " + d.availableDocks + "</td>" +
+			"</tr>" +
+			"<tr>" +
+				"<th> Total Docks</th> " +
+				" <td> " + d.totalDocks + "</td>" +
+			"</tr>" +
+			"<tr>" +
+				"<th>Location Status</th> " +
+				" <td> " + d.statusValue + "</td>" +
+			"</tr>" +
+			"<tr>" +
+				"<th> Available Bikes</th> " +
+				" <td> " + d.availableBikes + "</td>" +
+			"</tr>" +
+			"<tr>" +
+				"<th> Address</th> " +
+				" <td> " + "address here" + "</td>" +
+			"</tr>" 
+			);
+
+		}
+	
+	});
+
+})
+
+
+
+//Save row key in a local storage to pass it to the next page
+$(document).on("click", "#largerGroupInformation >li", function() {
+	rowid = $(this).closest("li").attr("li-id");
+        localStorage.setItem("rowid", $(this).closest("li").attr("li-id"));
+});
