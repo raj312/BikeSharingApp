0fcff9c3982d5f114cec4e2924b6424a38fe0f66
Raj Patel <raj312.raj312@gmail.com>
2018-03-28 20:12:50 -0400

    Show list view of all locations that have over 15 total docks

--- myscript.js
+++ myscript.js
@@ -1,4 +1,36 @@
 //Document ready function
 $(function() {
 
+	//get JSON data
+	$.getJSON('bikeshare.json', loadData);
+	
+
 }) //End of document ready
+
+function loadData(data){
+	var stationList = data.stationBeanList;
+	//loop through all the data and 
+
+	stationList.forEach(element => {
+		console.log(element.totalDocks);
+		
+		//Assume larger groups require more than 15 docks
+
+		if(element.totalDocks > 15) {
+			$("ul#largerGroupInformation").append(
+				"<li li-id='"+element.id+"'>" +
+					"<a href='#moreDetails'>  " +
+						"<h3>" + element.id + " - " + element.stationName + " </h3>" +
+					"</a>" +
+				"</li>");
+		}
+		
+	});
+
+	$("ul#largerGroupInformation").listview("refresh");
+	
+
+	//console.log(stationList);
+
+
+}
