2d84f99fb199f1bf9c27379d24eac579eb770ee2
Raj Patel <raj312.raj312@gmail.com>
2018-03-29 20:34:55 -0400

    Show well described info icons for a list of locations where available docks is greater than 30

--- myscript.js
+++ myscript.js
@@ -92,11 +92,19 @@ $(document).on("click", "#largerGroupInformation >li", function() {
 
 
 //display map
+var lat = 43.656246;
+var lng = -79.739509;
+
 $(document).on("pageshow", "#availDocks", function() {
 	if (navigator.geolocation){
-		lat = 43.66207;
-		lng = -79.37617;
-		// navigator.geolocation.getCurrentPosition(success,error);
+		//lat = 43.66207;
+		//lng = -80.37617;
+
+		navigator.geolocation.getCurrentPosition(pos => {
+			lat = pos.coords.latitude;
+			lng = pos.coords.longitude;
+		});
+
 		drawMap(lat,lng);
 	}
 	else {
@@ -110,73 +118,60 @@ function drawMap(lat,lng)
 {
 	var mapOptions = 
 		{
-			center: new google.maps.LatLng( lat, lng ), zoom: 12,   
+			center: new google.maps.LatLng( lat, lng ), zoom: 15,   
 						mapTypeId: google.maps.MapTypeId.ROADMAP   
 		};
+
 	var map = new google.maps.Map($("#map_canvas")[0],mapOptions);
+
+	//symbol representing current location
+	var lineSymbol = {
+		path: google.maps.SymbolPath.CIRCLE,
+		scale: 8,
+		strokeColor: '#393'
+	};
 
 	var myMarker = new google.maps.Marker ({
 		map: map,animation: google.maps.Animation.DROP,
+		icon: lineSymbol,
 		position: new google.maps.LatLng(lat, lng)
 	});
-		
-	var info = new google.maps.InfoWindow ( {content: "My current location", maxWidth: 150  } );
-	
-	google.maps.event.addListener(myMarker, "click", function() {info.open(map, myMarker);});
-	
+
 	var geocoder = new google.maps.Geocoder();
+
+	var info = new google.maps.InfoWindow ( {content: "My current location", maxWidth: 150  } );
+
+	google.maps.event.addListener(myMarker, "click", function() {info.open(map, myMarker);});
+
 	geocoder.geocode({"latLng":new google.maps.LatLng(lat, lng)},function(data,status) {
-		info.setContent(data[0].formatted_address);
+		if(data != null) {
+			info.setContent(data[0].formatted_address);
+		}
 	});
-	
-	/*
-	var MajorCities = [ 
-		new google.maps.LatLng(43.653226,-79.38318429999998),
-		new google.maps.LatLng(43.7764258, -79.2317521),
-		new google.maps.LatLng(43.5890452,-79.6441198)
-	];
-	
-	var majorCitiesRoute = new google.maps.Polyline( {path: MajorCities,strokeColor: "#FF0000",strokeWeight: 2 });      majorCitiesRoute.setMap(map);
-	
-	majorCitiesRoute.addListener('click',showMarker);
-	function showMarker(event) 
-	{
-		var myMarker = new google.maps.Marker({map: map,animation: google.maps.Animation.DROP,position: event.latLng }); 
-		var info = new google.maps.InfoWindow ({ maxWidth: 150 } );
-		google.maps.event.addListener(myMarker,"click",function(event){
-			var lat = event.latLng.lat();
-			var lng = event.latLng.lng();
-			geocoder.geocode({"latLng":new google.maps.LatLng(lat, lng)}, 
-							 function(data,status){
-								info.setContent(data[0].formatted_address);});
-								info.open(map, myMarker);   
+
+	jsonData.forEach(l => {
+		if(l.availableDocks > 20){
+			var aMarker = new google.maps.Marker ({
+				map: map,animation: google.maps.Animation.DROP,
+				position: new google.maps.LatLng(l.latitude, l.longitude)
 			});
-	}
-	
-	var polyline = new google.maps.Polyline({
-		strokeColor: "#FF0000",strokeWeight: 2 
-		});
-	polyline.setMap(map);
-	map.addListener('click', addLatLng);
-	function addLatLng(event) 
-	{
-		var path = polyline.getPath();
-		path.push(event.latLng);
-		var marker = new google.maps.Marker({
-			position: event.latLng,title: '#' + path.getLength(),map: map
-		});
 		
-		var info = new google.maps.InfoWindow ({
-			maxWidth: 150});
-		google.maps.event.addListener(marker, "click", function(){
-			info.open(map, marker);
-			lat = marker.getPosition().lat();
-			lng = marker.getPosition().lng();
-			var geocoder = new google.maps.Geocoder();
-			geocoder.geocode({"latLng":new google.maps.LatLng(lat,lng)},function(data,status) {info.setContent(data[0].formatted_address);                                     
-			});
-		});
-	}*/
+			var aInfo = new google.maps.InfoWindow ( 
+				{content: l.stationName + " has " + l.availableDocks + " available docks", maxWidth: 150  } 
+			);
+			
+			google.maps.event.addListener(aMarker, "click", function() {aInfo.open(map, aMarker);});
+
+			//formatting address
+			geocoder.geocode(
+				{"latLng":new google.maps.LatLng(l.latitude, l.longitude)},function(adata,status) {
+					if(adata != null){
+						aInfo.setContent(adata[0].formatted_address + " Available docks: " + l.availableDocks);
+					}
+			}); 
+			
+		}
+	});
 
 }
 
@@ -185,8 +180,9 @@ function success(pos)
 {
 	lat = pos.coords.latitude;
 	lng = pos.coords.longitude;
-	alert(lat + " " + lng);
-	drawMap(lat,lng)
+//	alert(lat + " " + lng);
+//	drawMap(lat,lng)
+	alert("Hi");
 };
 		
 //error
@@ -201,3 +197,27 @@ function error(err) {
 function initMap(){
 	alert("yp");
 }
+
+//add multiple markers
+function addMarkers(){
+
+	jsonData.forEach(l => {
+		if(l.availableDocks > 0){
+			var aMarker = new google.maps.Marker ({
+				map: map,animation: google.maps.Animation.DROP,
+				position: new google.maps.LatLng(l.latitude, l.longitude)
+			});
+		
+			var ainfo = new google.maps.InfoWindow ( 
+				{content: "Available Docks" + l.availableDocks, maxWidth: 150  } 
+			);
+			
+			google.maps.event.addListener(aMarker, "click", function() {info.open(map, aMarker);});
+		}
+
+//		console.log(l.stationName + " " + l.latitude + " " + l.longitude);
+	});
+
+
+}
+
